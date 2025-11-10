b4ba6bd09a34ee8fc1fe6bb521827ffa06d9a4b2
vincentbastille10 <135065221+vincentbastille10@users.noreply.github.com>
2025-11-10 12:54:27 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 # api/index.py
 from vercel_wsgi import handle
-from app import app  # ton app Flask: app = Flask(__name__)
+from app import app  # ton app Flask (app = Flask(__name__))
 
 def handler(event, context):
     return handle(app, event, context)
