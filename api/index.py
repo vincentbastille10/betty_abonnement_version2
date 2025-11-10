0018d007875cf9aee44823faddc89d307cf9d01e
vincentbastille10 <135065221+vincentbastille10@users.noreply.github.com>
2025-11-10 12:33:07 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,3 +1,6 @@
-import os
-from app import app as application
-os.environ.setdefault("FLASK_ENV", "production")
+# api/index.py
+from vercel_wsgi import handle
+from app import app  # ton app Flask principale (app = Flask(__name__))
+
+def handler(event, context):
+    return handle(app, event, context)
